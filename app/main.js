2ae82dda6371a7c8568013985633ac6eb2e46a14
Raminx524 <Raminx524@Gmail.com>
2024-05-27 13:39:18 +0300

    Added Search functionality

--- app/main.js
+++ app/main.js
@@ -1,43 +1,48 @@
 const booksUrl = "http://localhost:8001/books";
+const historyUrl = "http://localhost:8001/history";
 let numPage = 1;
 const prevBtn = document.querySelector("#previousHanler");
 const nextBtn = document.querySelector("#nextHanler");
 const prevNextDIvElem = document.querySelector("#pagination");
 const createFormElem = document.querySelector("#createBookForm");
 const bookDetailsContainer = document.querySelector("#bookDetailsDisplay");
-// let activeBookId;
-async function renderBooks() {
+
+async function renderAll() {
   try {
     const res = await axios.get(`${booksUrl}?_page=${numPage}&_per_page=20`);
     const books = res.data.data;
-    const tableElem = document.querySelector("table");
-    tableElem.innerHTML = `<thead>
-    <th>ID</th>
-    <th>TITLE</th>
-    </thead><tbody></tbody>`;
-    const tbodyElem = tableElem.querySelector("tbody");
-    for (let i = 0; i < books.length; i++) {
-      tbodyElem.innerHTML += `
-      <tr onclick="goToBook(this)">
-        <td>${books[i].id}</td>
-        <td>${books[i].title}</td>
-        </tr>`;
-    }
-    prevNextDIvElem.style.display = "flex";
+    renderBooks(books);
     paginationHandler(res.data);
   } catch (error) {
     console.log(error);
   }
 }
 
+function renderBooks(books) {
+  const tableElem = document.querySelector("table");
+  tableElem.innerHTML = `<thead>
+    <th>ID</th>
+    <th>TITLE</th>
+    </thead><tbody></tbody>`;
+  const tbodyElem = tableElem.querySelector("tbody");
+  for (let i = 0; i < books.length; i++) {
+    tbodyElem.innerHTML += `
+      <tr onclick="goToBook(this)">
+        <td>${books[i].id}</td>
+        <td>${books[i].title}</td>
+        </tr>`;
+  }
+  prevNextDIvElem.style.display = "flex";
+}
+
 prevBtn.onclick = () => {
   numPage--;
-  renderBooks();
+  renderAll();
 };
 
 nextBtn.onclick = () => {
   numPage++;
-  renderBooks();
+  renderAll();
 };
 
 function paginationHandler(pagesInfo) {
@@ -64,7 +69,7 @@ createFormElem.addEventListener("submit", async (e) => {
     });
     msgBox.innerText = "Book added successfully!";
     msgBox.style.color = "green";
-    renderBooks();
+    renderAll();
   } catch (error) {
     console.log(error);
     msgBox.innerText = "Failure!";
@@ -72,30 +77,24 @@ createFormElem.addEventListener("submit", async (e) => {
   }
 });
 
-// function showDetails(elem) {
-//   bookDetailsContainer.style.display = "flex";
-//   activeBookId = elem.childNodes[1].innerText;
-//   axios.get(`${booksUrl}/${activeBookId}`).then((res) => {
-//     const bookData = res.data;
-//     const valuesElems = document.querySelectorAll(".infoValue");
-//     let elemIndex = 0;
-//     for (let key in bookData) {
-//       if (key == "image") {
-//         continue;
-//       }
-//       if (key == "ISBN") {
-//         valuesElems[elemIndex].innerText = bookData[key]["identifier"];
-//       } else {
-//         valuesElems[elemIndex].innerText = bookData[key];
-//       }
-//       elemIndex++;
-//     }
-//   });
-// }
-
 function goToBook(elem) {
   const bookID = elem.childNodes[1].innerText;
   window.location.assign(`./bookdetails.html?id=${bookID}`);
 }
 
-renderBooks();
+async function searchBook() {
+  const bookToSearch = document.querySelector("#bookSearchValue").value;
+  try {
+    const response = await axios.get(`${booksUrl}?_page=1&_per_page=50`);
+    console.log(response.data);
+    const currentBooks = response.data.data;
+    const filteredBooks = currentBooks.filter((book) =>
+      book.title.includes(bookToSearch)
+    );
+    renderBooks(filteredBooks);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+renderAll();
